4bab06f9d1ba20ca99676bebe84c6b99fff43533
jmvalino <jmvalinowapx@gmail.com>
2018-10-01 22:46:25 +0800

    package update

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.use(express.static('client/build'))
 
 if (process.env.NODE_ENV === 'production') {
     const path = require('path');
-    app.get('/*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendfile(path.resolve(__dirname, '../client', 'build', 'index.html'))
     })
 }
