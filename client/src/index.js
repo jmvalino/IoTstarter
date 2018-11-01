48a99ee0304a725306651a1991f92ba7c8581b4e
jmvalino <jmvalinowapx@gmail.com>
2018-11-01 22:22:58 +0800

    updated landing page

--- client/src/index.js
+++ client/src/index.js
@@ -14,17 +14,27 @@ class App extends Component {
     render() {
         return (
             <React.Fragment>
-                <Card
-                    style={{ width: 300 }}
-                    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-                    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
-                >
-                    <Meta
-                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                        title="Card title"
-                        description="This is the description"
-                    />
-                </Card>
+                <div style={{display: 'flex', textAlign: 'center', flexDirection: 'column'}}>
+                    <div style={{flex: 1, display: 'flex', flexDirection: 'row'}}>
+
+                        <div style={{flex: 1,  color: '#fff',background: '#000',}}>
+                        <img src={PowerImage} width = '200'/>
+                        <strong>May Power Ga?</strong>
+                        <p> An Ecosystem of hassle-power Power Outage Management</p>
+                        </div>
+                        <div style={{flex: 1, background: '#fff', color: 'gray'}}>
+                        <h1><Icon type="compass" theme="twoTone" /> Outage Locator</h1>
+                        
+                        <input type="text" placeholder='search location'/>
+                        </div>
+                    </div>
+                    <div style={{flex: 1, background: '', borderTop: '1px solid #555'}}>
+                    <h1><Icon type="download" theme="outlined" /> Download the App (23MB)</h1>
+                    <strong><a href="http://g.co">com.valinotech.maypowerga.apk</a></strong>
+                    </div>
+
+                
+                </div>
             </React.Fragment>
 
         )
